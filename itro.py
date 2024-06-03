e8a01bd00e42c64c089032aa1a80cacf913c7e31
Ali-Hasnain-G <mybloggers93@gmail.com>
2024-06-03 12:27:06 +0500

    git remotr

---
+++ itro.py
@@ -0,0 +1,12 @@
+ # this practise file for git 
+name = "ali"
+height = 139
+weight = 70
+bmi = height * weight
+print(bmi)
+if bmi > 10000:
+    print("greater than")
+elif bmi >= 9730:
+    print("equal")
+else:
+    print('less than')
