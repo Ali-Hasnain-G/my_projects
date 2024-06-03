aa30a74d0b4d47b2c2e97a58d35c43085fdf84ed
Ali-Hasnain-G <mybloggers93@gmail.com>
2024-06-03 20:36:44 +0500

    update

--- New_api_project.py
+++ New_api_project.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify
-
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return jsonify({'message': 'Welcome to my API!'})
+def name():
+    return jsonify('your name is this')
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
