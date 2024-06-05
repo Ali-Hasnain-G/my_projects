9c5cdf98064e1c34481565cf3f6d9e568e688599
Ali-Hasnain-G <mybloggers93@gmail.com>
2024-06-05 15:19:22 +0500

    new

---
+++ prep.sh
@@ -0,0 +1,11 @@
+npx create-expo-app coding t expo-template-blank-typescript
+
+cd./coding
+
+npx expo install firebase
+
+#Navigation
+
+npnpx m install @react-navigation/native @react-navigation/native-stack
+
+npx expo install react-native-screens react-native-safe-area-context
